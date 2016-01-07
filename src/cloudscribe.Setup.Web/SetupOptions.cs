3e05c21b7f2c2ffddabd7bf32abdb4c724e17a41
Joe Audette <joeaudette@users.noreply.github.com>
2016-01-07 14:20:12 -0500

    SetupOptions belongs in this project
    moved from cloudscribe.Core.Models

---
+++ src/cloudscribe.Setup.Web/SetupOptions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Source Tree Solutions, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Author:					Joe Audette
+// Created:					2015-09-04
+// Last Modified:			2016-01-07
+// 
+
+namespace cloudscribe.Setup.Web
+{
+    public class SetupOptions
+    {
+        public SetupOptions()
+        { }
+
+        public string ConfigBasePath { get; set; } = "/cloudscribe_config";
+        public string InstallScriptPathFormat { get; set; } = "/cloudscribe_config/applications/{0}/install/{1}";
+        public string UpgradeScriptPathFormat { get; set; } = "/cloudscribe_config/applications/{0}/upgrade/{1}";
+        public bool TryToCreateMsSqlDatabase { get; set; } = false;
+        public bool AllowAnonymous { get; set; } = true; // needs to be true for initial setup but can be seto to false as long as you login before upgrading
+        public bool ShowSchemaListOnSetupPage { get; set; } = true;
+        public bool ProbeSystemOnStatusPage { get; set; } = true;
+        public bool ShowErrors { get; set; } = true;
+        public bool ShowConnectionError { get; set; } = false;
+        public bool TryAnywayIfFailedAlterSchemaTest { get; set; } = false;
+        public string SetupHeaderConfigPath { get; set; } = "/cloudscribe_config/setup/SetupHeader.config";
+        public string SetupHeaderConfigPathRtl { get; set; } = "/cloudscribe_config/setup/SetupHeader-rtl.config";
+
+
+    }
+}
